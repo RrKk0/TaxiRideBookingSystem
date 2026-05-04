95977fb5f333b2a9ffea5185d3e17d94765cdfaa
Rana0767 <447005628@pnu.edu.sa>
2026-05-04 14:25:01 +0300

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,40 @@
+public class Driver extends Person {
+    private int experience;
+    private int age;
+
+    public Driver() {
+    }
+
+    public Driver(String name, String phone, int id, int experience, int age) {
+        super(name, phone, id);
+        this.experience = experience;
+        this.age = age;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void drive() {
+        System.out.println("Driver is driving...");
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Experience: " + experience);
+        System.out.println("Age: " + age);
+    }
+}
