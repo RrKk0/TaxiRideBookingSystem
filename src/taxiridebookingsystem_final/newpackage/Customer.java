af7f756976d28f73ed5c6425fb30c71e4cf23c6f
samsung <samsung@DESKTOP-6M6GSOK>
2026-05-04 19:09:59 +0300

    update

--- src/taxiridebookingsystem_final/newpackage/Customer.java
+++ src/taxiridebookingsystem_final/newpackage/Customer.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package taxiridebookingsystem_final.newpackage;
 
-/**
- *
- * @author PC
- */
-public class Customer {
-    
+public final class Customer extends Person {
+
+    private String email;
+    private double rating;
+
+    public Customer() {
+    }
+
+    public Customer(String name, String phone, int id, String email, double rating) {
+        super(name, phone, id);
+        this.email = email;
+        this.rating = rating;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Name: " + getName());
+        System.out.println("Phone: " + getPhone());
+        System.out.println("ID: " + getId());
+        System.out.println("Email: " + email);
+        System.out.println("Rating: " + rating);
+    }
 }
