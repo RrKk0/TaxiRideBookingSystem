204d01ee7dcee04a65961e1fda8905c5396e649f
samsung <samsung@DESKTOP-6M6GSOK>
2026-05-04 19:10:11 +0300

    update

--- src/taxiridebookingsystem_final/newpackage/Driver.java
+++ src/taxiridebookingsystem_final/newpackage/Driver.java
@@ -1,13 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package taxiridebookingsystem_final.newpackage;
 
-/**
- *
- * @author PC
- */
-public class Driver {
-    
+public final class Driver extends Person {
+
+    private int experience;
+    private int age;
+
+    public Driver() {
+    }
+
+    public Driver(String name, String phone, int id, int experience, int age) {
+        super(name, phone, id);
+        this.experience = experience;
+        this.age = age;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void drive() {
+        System.out.println("Driver is driving...");
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Name: " + getName());
+        System.out.println("Phone: " + getPhone());
+        System.out.println("ID: " + getId());
+        System.out.println("Experience: " + experience);
+        System.out.println("Age: " + age);
+    }
 }
