f0b219f3856593964cfc4d4f871a9c50e8f7106f
PC <PC@LAPTOP-HL91BAOH>
2026-05-05 21:03:38 +0300

    add all project classes

--- src/taxiridebookingsystem_final/newpackage/Bookable.java
+++ src/taxiridebookingsystem_final/newpackage/Bookable.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package taxiridebookingsystem_final.newpackage;
 
-/**
- *
- * @author PC
- */
-public class Bookable {
-    
+public interface Bookable {
+  
+
+    public abstract void cancel();
+      public abstract void book();
 }
+
