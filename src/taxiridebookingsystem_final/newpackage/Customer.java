e4f91816f2a63adfa5888885b15dc9bf4328a9a2
PC <PC@LAPTOP-HL91BAOH>
2026-05-08 19:37:35 +0300

    add all project classes

--- src/taxiridebookingsystem_final/newpackage/Customer.java
+++ src/taxiridebookingsystem_final/newpackage/Customer.java
@@ -9,9 +9,10 @@ public final class Customer extends Person {
     }
 
     public Customer(String name, String phone, int id, String email, double rating) {
+
         super(name, phone, id);
-        this.email = email;
-        this.rating = rating;
+        setEmail(email);
+        setRating(rating);
     }
 
     public String getEmail() {
@@ -19,7 +20,12 @@ public final class Customer extends Person {
     }
 
     public void setEmail(String email) {
-        this.email = email;
+
+        if (email == null || !email.contains("@")) {
+            System.out.println("Invalid email!");
+        } else {
+            this.email = email;
+        }
     }
 
     public double getRating() {
@@ -27,11 +33,17 @@ public final class Customer extends Person {
     }
 
     public void setRating(double rating) {
-        this.rating = rating;
+
+        if (rating < 0 || rating > 5) {
+            System.out.println("Rating must be between 0 and 5!");
+        } else {
+            this.rating = rating;
+        }
     }
 
     @Override
     public void displayInfo() {
+
         System.out.println("Name: " + getName());
         System.out.println("Phone: " + getPhone());
         System.out.println("ID: " + getId());
