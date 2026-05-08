2e6378283d7554c6a0e89d0aa1bd74c83d3ac488
PC <PC@LAPTOP-HL91BAOH>
2026-05-08 15:14:28 +0300

    add all project classes

--- src/taxiridebookingsystem_final/newpackage/Bookable.java
+++ src/taxiridebookingsystem_final/newpackage/Bookable.java
@@ -2,8 +2,7 @@
 package taxiridebookingsystem_final.newpackage;
 
 public interface Bookable {
-  
-
+  public final static double FEE=10.0;
     public abstract void cancel();
       public abstract void book();
 }
