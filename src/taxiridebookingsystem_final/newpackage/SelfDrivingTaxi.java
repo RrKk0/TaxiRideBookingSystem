646d68cf429e6baddd2d54dd79629a2a501e43ce
PC <PC@LAPTOP-HL91BAOH>
2026-05-08 15:32:32 +0300

    add all project classes

--- src/taxiridebookingsystem_final/newpackage/SelfDrivingTaxi.java
+++ src/taxiridebookingsystem_final/newpackage/SelfDrivingTaxi.java
@@ -1,27 +1,31 @@
-
 package taxiridebookingsystem_final.newpackage;
 
 public class SelfDrivingTaxi extends Vehicle {
+
     private String softwareVersion;
     private String sensorStatus;
 
-    public SelfDrivingTaxi( String model, String licensePalet, int capacity,String softwareVersion, String sensorStatus) {
+    public SelfDrivingTaxi(String model, String licensePalet, int capacity, String softwareVersion, String sensorStatus) {
         super(model, licensePalet, capacity);
-        this.softwareVersion = softwareVersion;
+        setSoftwareVersion(softwareVersion);
         this.sensorStatus = sensorStatus;
     }
 
     public SelfDrivingTaxi() {
-        this("","",0,"","");
+        this("", "", 0, "", "");
     }
-
 
     public String getSoftwareVersion() {
         return softwareVersion;
     }
 
     public void setSoftwareVersion(String softwareVersion) {
-        this.softwareVersion = softwareVersion;
+
+        if (softwareVersion != null && !softwareVersion.isEmpty()) {
+            this.softwareVersion = softwareVersion;
+        } else {
+            this.softwareVersion = "Unknown";
+        }
     }
 
     public String getSensorStatus() {
@@ -29,22 +33,27 @@ public class SelfDrivingTaxi extends Vehicle {
     }
 
     public void setSensorStatus(String sensorStatus) {
-        this.sensorStatus = sensorStatus;
+
+        if (sensorStatus != null && !sensorStatus.isEmpty()) {
+            this.sensorStatus = sensorStatus;
+        } else {
+            this.sensorStatus = "Inactive";
+        }
     }
 
     @Override
     public String toString() {
-        return super.toString()+"SelfDrivingTaxi{" + "softwareVersion=" + softwareVersion + ", sensorStatus=" + sensorStatus + '}';
+        return super.toString() + "SelfDrivingTaxi{" + "softwareVersion=" + softwareVersion + ", sensorStatus=" + sensorStatus + '}';
     }
 
-   public void driveAutomatically(){
-       System.out.println("The self driving taxi is now driving automatically");
-  
-   }
-   
-   @Override
-   public double calculateFare(double distance){
-    return super.calculateFare(distance) +10;
+    public void driveAutomatically() {
+        System.out.println("The self driving taxi is now driving automatically");
+
     }
-    
+
+    @Override
+    public double calculateFare(double distance) {
+        return super.calculateFare(distance) + 10;
+    }
+
 }
