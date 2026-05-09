0c09a2aa44305e0bf86c78a0c6d4fc6b36f6feaf
PC <PC@LAPTOP-HL91BAOH>
2026-05-10 01:19:36 +0300

    add all project classes

---
+++ src/taxiridebookingsystem_final/newpackage/InvalidBookingException.java
@@ -0,0 +1,8 @@
+package taxiridebookingsystem_final.newpackage;
+
+public class InvalidBookingException extends Exception {
+
+    public InvalidBookingException(String message) {
+        super(message);
+    }
+}
